13c81512972990317c4e87c467f7cf55f42e57dd
Liteskarr <Kotvergki2019>
2021-07-12 13:38:34 +0300

    refactor(dadata_parser.py)

--- src/dadata_parser.py
+++ src/dadata_parser.py
@@ -12,16 +12,18 @@ PARTY_NAME = 'party'
 def entity_from_json(data: dict) -> Entity:
     return Entity(
         Name=data['value'],
-        INN=int(data['data']['inn']),
-        OGRN=int(data['data']['ogrn']),
-        IsActive=data['data']['state']['status'] == 'ACTIVE'
+        FullName=data['data']['name']['full_with_opf'],
+        INN=data['data']['inn'],
+        OGRN=data['data']['ogrn'],
+        Address=data['data']['address']['value'],
+        IsActive=data['data']['state']['status'] == 'ACTIVE',
     )
 
 
 class EntitiesByInnGetter(QObject):
-    inn_processed = pyqtSignal(int, int)  # INN, it
+    entity_processed = pyqtSignal(Entity, int, int)  # entity, number, count
 
-    def __init__(self, inns: list[Entity], **filters):
+    def __init__(self, inns: list[str], **filters):
         super().__init__()
         self._inns = inns
         self._filters = filters
@@ -32,10 +34,13 @@ class EntitiesByInnGetter(QObject):
         for it, inn in enumerate(self._inns):
             self.inn_processed.emit(inn, it)
             try:
-                response = dadata.find_by_id(PARTY_NAME, str(inn), **self._filters)
+                response = dadata.find_by_id(PARTY_NAME, inn, **self._filters)
                 if response:
                     result.append(entity_from_json(response[0]))
             except Exception:
                 pass
         dadata.close()
         return result
+
+    def process(self):
+        return self.__call__()
