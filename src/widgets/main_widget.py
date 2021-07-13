a4eb54a59b9966f1d248d5ee5682d133a78afdd2
Liteskarr <Kotvergki2019>
2021-07-13 14:32:03 +0300

    update(main_widget.py): adding filters, type filters, multiply loading

--- src/widgets/main_widget.py
+++ src/widgets/main_widget.py
@@ -1,67 +1,103 @@
-import os
+import locale
+from decimal import Decimal
+from datetime import datetime
 
 from PyQt5.QtWidgets import (QWidget,
-                             QFileDialog)
-from PyQt5.QtCore import (pyqtSignal)
+                             QFileDialog, QListWidgetItem)
 from PyQt5.uic import loadUi
-from docxtpl import DocxTemplate
 
+from background_process import BackgroundProcess, BackgroundProcessArgs
 from widgets.working_dialog import WorkingDialog
-from dadata_parser import get_entities_by_inn
-from data_parser import parse_file
-from builder import build
-from worker import Worker
-
-
-class ResultPrinter(Worker):
-    data_loaded = pyqtSignal()
-    data_checked = pyqtSignal()
-    data_printed = pyqtSignal()
-
-    def __init__(self, data_path: str, template_path: str, output_path: str):
-        super().__init__(self._target, data_path, template_path, output_path)
-
-    def _target(self, data_path, template_path, output_path):
-        records = list(parse_file(data_path))
-        inns = list(set(map(lambda x: x.TargetINN, records)))
-        self.data_loaded.emit()
-        entities = get_entities_by_inn(inns, type='LEGAL')
-        self.data_checked.emit()
-        for entity in entities:
-            if not entity.IsActive:
-                continue
-            r = list(filter(lambda x: x.TargetINN == entity.INN, records))
-            build(r, {}, DocxTemplate(template_path), os.path.join(output_path, f'{entity.INN}.docx'))
-        self.data_printed.emit()
+from entity import Entity
 
 
 class MainWidget(QWidget):
+    RECORDS_FILTERS = {
+        'Претензия': lambda r: r.Credit == Decimal('0'),
+        'Запрос': lambda r: r.Debit == Decimal('0'),
+        'Претензия с запросом': lambda r: True,
+    }
+
+    ENTITIES_TYPE_FILTERS = {
+        'ИП': {'type': 'LEGAL'},
+        'ЮЛ': {'type': 'INDIVIDUAL'},
+        'ВСЕ': {}
+    }
+
     def __init__(self):
         super().__init__()
-        self.template_path = ''
-        self.data_path = ''
-        self.dir_path = ''
-        self.dialog: WorkingDialog = None
+        # Paths
+        self.template_path: str = ''
+        self.data_paths: list[str] = []
+        self.dir_path: str = ''
+        # Settings
+        self.current_filter = self.RECORDS_FILTERS['Претензия']
+        self.current_type_filter = self.ENTITIES_TYPE_FILTERS['ВСЕ']
+        self.date = ''
+        self.number = ''
+        # Other widgets
+        self.dialog = WorkingDialog(self)
         self._configure_ui()
+        self.set_today_date()
 
     def _configure_ui(self):
         loadUi('res/main_widget.ui', self)
+        # Non-settings buttons
         self.load_template_button.clicked.connect(self.load_template)
-        self.load_data_button.clicked.connect(self.load_data)
+        self.add_data_button.clicked.connect(self.add_data)
+        self.delete_data_button.clicked.connect(self.delete_data)
         self.choose_dir_button.clicked.connect(self.choose_dir)
         self.print_result_button.clicked.connect(self.print_result)
+        # Filters
+        self.claim_radio.toggled.connect(lambda: self.change_filter(self.claim_radio.text()))
+        self.request_radio.toggled.connect(lambda: self.change_filter(self.request_radio.text()))
+        self.claim_request_radio.toggled.connect(lambda: self.change_filter(self.claim_request_radio.text()))
+        # Type Filters
+        self.legal_radio.toggled.connect(lambda: self.change_type_filter(self.legal_radio.text()))
+        self.individual_radio.toggled.connect(lambda: self.change_type_filter(self.individual_radio.text()))
+        self.all_radio.toggled.connect(lambda: self.change_type_filter(self.all_radio.text()))
+        # Date
+        self.date_line.editingFinished.connect(self.change_date)
+        # Number
+        self.number_line.editingFinished.connect(self.change_number)
         self._update_ui()
 
+    def change_date(self):
+        self.date = self.date_line.text()
+
+    def set_today_date(self):
+        locale.setlocale(locale.LC_ALL, 'ru_RU')
+        self.date_line.setText(datetime.now().strftime('%d %B %Y'))
+        locale.setlocale(locale.LC_ALL, 'en_EN')
+        self.change_date()
+
+    def change_number(self):
+        self.number = self.number_line.text()
+
+    def change_type_filter(self, text: str):
+        self.current_type_filter = self.ENTITIES_TYPE_FILTERS[text]
+
+    def change_filter(self, text: str):
+        self.current_filter = self.RECORDS_FILTERS[text]
+
     def load_template(self):
-        self.template_path = QFileDialog.getOpenFileName(self, 'Выбор файла', '', '', '')[0]
+        self.template_path = QFileDialog.getOpenFileName(self, 'Выбор файла', '', '*.docx')[0]
         if self.template_path:
             self.template_line.setText(self.template_path)
         self._update_ui()
 
-    def load_data(self):
-        self.data_path = QFileDialog.getOpenFileName(self, 'Выбор файла', '', '', '')[0]
-        if self.data_path:
-            self.data_line.setText(self.data_path)
+    def add_data(self):
+        data_path = QFileDialog.getOpenFileName(self, 'Выбор файла', '', '*.xlsx')[0]
+        if data_path and data_path not in self.data_paths:
+            self.data_paths.append(data_path)
+            self.data_list.addItem(QListWidgetItem(data_path))
+        self._update_ui()
+
+    def delete_data(self):
+        deleted_item = self.data_list.currentItem()
+        if deleted_item:
+            self.data_paths.remove(deleted_item.text())
+            self.data_list.takeItem(self.data_list.currentRow())
         self._update_ui()
 
     def choose_dir(self):
@@ -70,26 +106,39 @@ class MainWidget(QWidget):
             self.dir_line.setText(self.dir_path)
         self._update_ui()
 
-    def handle_data_loading(self):
-        self.working_dialog.set_state('Сбор данных организаций', 50)
+    def _handle_starting(self):
+        self.dialog.show()
+        self.dialog.set_state('Начало работы', percent=0)
 
-    def handle_data_checking(self):
-        self.working_dialog.set_state('Распечатка претензий', 75)
+    def _handle_file_reading(self, filename: str):
+        self.dialog.set_state(f'Чтение файла: {filename}', percent=50)
 
-    def handle_data_printing(self):
-        self.working_dialog.set_state('Завершено!', 100)
-        self.working_dialog.close()
-        self.working_dialog = None
+    def _handle_entity_processing(self, entity: Entity, number: int, count: int):
+        self.dialog.set_state(f'Сбора данных по ИНН: {entity.INN}', percent=int(100 * number / count))
+
+    def _handle_entity_printing(self, entity: Entity, number: int, count: int):
+        self.dialog.set_state(f'Распечатка данных по ИНН: {entity.INN}', percent=int(100 * number / count))
+
+    def _handle_finishing(self):
+        self.dialog.set_state('', percent=0)
+        self.dialog.hide()
 
     def print_result(self):
-        self.working_dialog = WorkingDialog(self)
-        self.working_dialog.show()
-        self.working_dialog.set_state('Загрузка данных', 25)
-        printer = ResultPrinter(self.data_path, self.template_path, self.dir_path)
-        printer.data_loaded.connect(self.handle_data_loading)
-        printer.data_checked.connect(self.handle_data_checking)
-        printer.data_printed.connect(self.handle_data_printing)
-        printer.run()
+        args = BackgroundProcessArgs(
+            self.data_paths,
+            self.template_path,
+            self.dir_path,
+            self.number,
+            self.date,
+            self.current_filter
+        )
+        process = BackgroundProcess(args)
+        process.started.connect(self._handle_starting)
+        process.file_read.connect(self._handle_file_reading)
+        process.entity_processed.connect(self._handle_entity_processing)
+        process.entity_printed.connect(self._handle_entity_printing)
+        process.finished.connect(self._handle_finishing)
+        process.run()
 
     def _update_ui(self):
-        self.print_result_button.setEnabled(bool(self.dir_path) and bool(self.data_path) and bool(self.template_path))
+        self.print_result_button.setEnabled(bool(self.dir_path) and bool(self.data_paths) and bool(self.template_path))
